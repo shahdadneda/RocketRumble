ef5d21966544acf65dcee94d839f0519369f2cf6
Shahdad Neda <shahdad.neda@gmail.com>
2024-09-13 21:47:35 -0700

    made code tidy with extract method

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -40,6 +40,16 @@ public class Movement : MonoBehaviour
             return;
         }
         if (Input.GetKey(KeyCode.Space) ) {
+            StartThrusting();
+        }
+        //stop everything if space is not being pressed
+        else StopThrusting();
+
+
+
+
+        void StartThrusting()
+        {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
             if (!audioSource.isPlaying)
             {
@@ -51,16 +61,16 @@ public class Movement : MonoBehaviour
             {
                 mainThrustParticle.Play();
             }
-            
-        }
-        //stop everything if space is not being pressed
-        else {
-            audioSource.Stop();
-            mainThrustParticle.Stop();
         }
 
     }
-    
+
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainThrustParticle.Stop();
+    }
+
     void ProcessRotation(){
         //only litsen to everything below if we have not won won or crashed, if we have then return
         if (collisionHandler.hasWon == true || collisionHandler.hasCrashed == true)
@@ -69,40 +79,55 @@ public class Movement : MonoBehaviour
         }
 
         //what to do when A is being pressed
-        if (Input.GetKey(KeyCode.A)){
-            ApplyRotation(sideThrust);
-
-            //play right particle thrust if it is not already being played (its not)
-            if (!rightParticle.isPlaying)
-            {
-                rightParticle.Play();
-            }
-
-            // if it is being played, then stop the particles
-            else
-            {
-                rightParticle.Stop();
-                print("right stoping");
-            }
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateLeft();
 
         }
 
         //what to do when d is being pressed
-        else if (Input.GetKey(KeyCode.D)){
-            //same thing as 'a' rotation above 
-            ApplyRotation(-sideThrust);
-            if (!leftParticle.isPlaying)
-            {
-                leftParticle.Play();
-            }
-            else
-            {
-                leftParticle.Stop();
-                print("left stoping");
-            }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateRight();
 
         }
     }
+
+
+
+    private void RotateRight()
+    {
+        //same thing as 'a' rotation abo
+        ApplyRotation(-sideThrust);
+        if (!leftParticle.isPlaying)
+        {
+            leftParticle.Play();
+        }
+        else
+        {
+            leftParticle.Stop();
+            print("left stoping");
+        }
+    }
+
+    private void RotateLeft()
+    {
+        ApplyRotation(sideThrust);
+
+        //play right particle thrust if it is not already being played (its not)
+        if (!rightParticle.isPlaying)
+        {
+            rightParticle.Play();
+        }
+
+        // if it is being played, then stop the particles
+        else
+        {
+            rightParticle.Stop();
+            print("right stoping");
+        }
+    }
+
     private void ApplyRotation(float rotationThisFrame)
     {
         rb.freezeRotation = true; // freezing rotation so we can manually
