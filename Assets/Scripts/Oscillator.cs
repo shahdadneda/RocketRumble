60a92dc850e3596feafbaee8b5af2581766ba47c
Shahdad Neda <shahdad.neda@gmail.com>
2024-12-28 17:34:23 -0800

    added automatic occilation

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,21 +6,30 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
-
+    float movementFactor;
+    [SerializeField] float period = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
         startingPosition = transform.position;
-        // Debug.Log(startingPosition);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;  // will be continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283...
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2f;  // recalculated to go from 0 to 1 so its cleaner
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
+
+
     }
 }
  
